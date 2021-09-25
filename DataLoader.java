62891d7f569969812b87cb94952ef886351b6c4e
NiteshVish <nig301great@gmail.com>
2021-09-25 22:59:16 +0530

    add duration in task

--- DataLoader.java
+++ DataLoader.java
@@ -19,13 +19,15 @@ public class DataLoader{
 		// DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm a"); 
 		while (sc.hasNextLine()){
 	      	String[] data=sc.nextLine().split(",");
-	      	if(data.length==4 || data.length==3){
+	      	//sNo,path,type,time,duration
+	      	if(data.length==4 || data.length==5){
 	      		int num=0;
 	      		int iadd=0;
-	      		if(data.length==4) {num=Integer.valueOf(data[0]);iadd++;}
+	      		if(data.length==5) {num=Integer.valueOf(data[0]);iadd++;}
 	      		String t=data[2+iadd];
 	      		LocalDateTime date=LocalDateTime.parse(t,ft);
-	      		taskList.add(new Task(num,data[iadd],data[1+iadd],date));
+	      		int duration= Integer.valueOf(data[3+iadd]);
+	      		taskList.add(new Task(num,data[iadd],data[1+iadd],date,duration));
 	      	}
 	  	}
 	  	return taskList;
--- Model.java
+++ Model.java
@@ -8,21 +8,25 @@ class Task{
 	private String path;
 	private String type;
 	private LocalDateTime datetime;
-	public Task(int num,String path,String type,LocalDateTime datetime){
+	private int duration;
+	public Task(int num,String path,String type,LocalDateTime datetime,int duration){
 		this.num=num;
 		this.path=path;
 		this.datetime=datetime;
 		this.type=type;
+		this.duration=duration;
 	}
-	public Task(String path,String type,LocalDateTime datetime){
+
+	public Task(String path,String type,LocalDateTime datetime,int duration){
 		this.num=0;
 		this.path=path;
 		this.datetime=datetime;
 		this.type=type;
+		this.duration=duration;
 	}
 	@Override
 	public String toString(){
-		String res="Task \n num= "+num+"\npath= "+path+"\ndate= "+datetime+"\ntype= "+type;
+		String res="Task \n num= "+num+"\npath= "+path+"\ndate= "+datetime+"\ntype= "+type+" duration= "+duration;
 		return res;
 	}
 
